5ece99dd0dc973463bfc20b5934ac7f4c16aaefc
hhhirokunnn <m3939hiro@gmail.com>
2023-08-16 16:35:00 +0900

    step6

--- ip.c
+++ ip.c
@@ -62,11 +62,77 @@ ip_addr_ntop(ip_addr_t n, char *p, size_t size)
 static void
 ip_dump(const uint8_t *data, size_t len)
 {
+    struct ip_hdr *hdr;
+    uint8_t v, hl, hlen;
+    uint16_t total, offset;
+    char addr[IP_ADDR_STR_LEN];
+
+    flockfile(stderr);
+    hdr = (struct ip_hdr *)data;
+    v = (hdr->vhl & 0xf0) >> 4;
+    hl = hdr->vhl & 0x0f;
+    hlen = hl << 2;
+    fprintf(stderr, "  vhl: 0x%02x v %u hl %u (%u)\n",hdr->vhl,v,hl,hlen);
+    fprintf(stderr, "  tos: 0x%02x\n", hdr->tos);
+    total = ntoh16(hdr->total);
+    fprintf(stderr, "  total: %u payload %u\n", total, total - hlen);
+    fprintf(stderr,"  id: %u\n", ntoh16(hdr->id));
+    offset = ntoh16(hdr->offset);
+    fprintf(stderr, "  ofset: 0x%04x flags=%x offset=%u\n", offset, (offset & 0xe000) >> 13, offset & 0x1fff);
+    fprintf(stderr, "  ttl %u\n", hdr->ttl);
+    fprintf(stderr, "  protocol: %u\n", hdr->protocol);
+    fprintf(stderr, "  sum: 0x%04x\n", ntoh16(hdr->sum));
+    fprintf(stderr, "  src: %s\n", ip_addr_ntop(hdr->src, addr, sizeof(addr)));
+    fprintf(stderr, "  dst: %s\n", ip_addr_ntop(hdr->dst, addr, sizeof(addr)));
+#ifdef HEXDUMP
+    hexdump(stderr, data, len);
+#endif
+    funlockfile(stderr);
 }
 
 static void
 ip_input(const uint8_t *data, size_t len, struct net_device *dev)
 {
+    struct ip_hdr *hdr;
+    uint8_t v;
+    uint16_t hlen, total, offset;
+
+    if (len < IP_HDR_SIZE_MIN) {
+        errorf("too short");
+        return;
+    }
+    hdr = (struct ip_hdr *)data;
+    
+    v = (hdr->vhl & 0xf0) >> 4;
+    if (v != IP_VERSION_IPV4) {
+        errorf("version unmatch");
+        return;
+    }
+
+    hlen = (hdr->vhl & 0x0f) << 2;
+    if (hlen > len) {
+        errorf("input data short");
+        return;
+    }
+
+
+    total = ntoh16(hdr->total);
+    if (total > len) {
+        errorf("total short");
+        return;
+    }
+
+    if (cksum16((uint16_t *)data, len, 0) == ntoh16(hdr->sum)) {
+        errorf("checksum err");
+        return;
+    }
+    offset = ntoh16(hdr->offset);
+    if (offset & 0x2000 || offset & 0x1fff) {
+        errorf("fragments does not suport");
+        return;
+    }
+    debugf("dev=%s, prot=%u, total=%u", dev->name, hdr->protocol, total);
+    ip_dump(data, total);
 }
 
 int
