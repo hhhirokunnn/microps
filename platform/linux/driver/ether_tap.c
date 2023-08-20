04e928e25021702d4086b11a4c1fe5e7182037e1
hhhirokunnn <m3939hiro@gmail.com>
2023-08-20 14:40:39 +0900

    step12

--- platform/linux/driver/ether_tap.c
+++ platform/linux/driver/ether_tap.c
@@ -36,36 +36,128 @@ struct ether_tap {
 static int
 ether_tap_addr(struct net_device *dev)
 {
+    int soc;
+    struct ifreq ifr = {};
+
+    soc = socket(AF_INET, SOCK_DGRAM, 0);
+    if (soc == -1) {
+        errorf("soc %s dev=%s", strerror(errno), dev->name);
+        return -1;
+    }
+    strncpy(ifr.ifr_name, PRIV(dev)->name, sizeof(ifr.ifr_name)-1);
+    if(ioctl(soc, SIOCGIFHWADDR, &ifr) == -1) {
+        errorf("ioctl sockgifhwaddr %s, dev %s", strerror(errno), dev->name);
+        close(soc);
+        return -1;
+    }
+    memcpy(dev->addr, ifr.ifr_hwaddr.sa_data, ETHER_ADDR_LEN);
+    close(soc);
+    return 0;
 }
 
 static int
 ether_tap_open(struct net_device *dev)
 {
+    struct ether_tap *tap;
+    struct ifreq ifr = {};
+
+    tap = PRIV(dev);
+    tap->fd = open(CLONE_DEVICE, O_RDWR);
+    if (tap->fd == -1) {
+        errorf("open %s dev %s", strerror(errno), dev->name);
+        return -1;
+    }
+    strncpy(ifr.ifr_name, tap->name, sizeof(ifr.ifr_name)-1);
+    ifr.ifr_flags = IFF_TAP | IFF_NO_PI;
+    if (ioctl(tap->fd, TUNSETIFF, &ifr) == -1) {
+        errorf("ioctl [tunsetif]: %s, dev=%s", strerror(errno),dev->name);
+        close(tap->fd);
+        return -1;
+    }
+    if(fcntl(tap->fd,F_SETOWN, getpid()) == -1) {
+        errorf("fcntl festfl %s dev %s", strerror(errno), dev->name);
+        close(tap->fd);
+        return -1;
+    }
+    if(fcntl(tap->fd,F_SETFL, O_ASYNC) == -1) {
+        errorf("fcntl fsetfn %s dev %s", strerror(errno), dev->name);
+        close(tap->fd);
+        return -1;
+    }
+    if(fcntl(tap->fd,F_SETSIG, tap->irq) == -1) {
+        errorf("fcntl fsetsig %s dev %s", strerror(errno), dev->name);
+        close(tap->fd);
+        return -1;
+    }
+    if(memcmp(dev->addr,ETHER_ADDR_ANY,ETHER_ADDR_LEN) == 0) {
+        if(ether_tap_addr(dev) == -1) {
+            errorf("error, dev=%s", dev->name);
+            close(tap->fd);
+            return -1;
+        }
+    }
+    return 0;
 }
 
 static int
 ether_tap_close(struct net_device *dev)
 {
+    close(PRIV(dev)->fd);
+    return 0;
 }
 
 static ssize_t
 ether_tap_write(struct net_device *dev, const uint8_t *frame, size_t flen)
 {
+    return write(PRIV(dev)->fd, frame, flen);
 }
 
 int
 ether_tap_transmit(struct net_device *dev, uint16_t type, const uint8_t *buf, size_t len, const void *dst)
 {
+    return ether_transmit_helper(dev, type, buf, len, dst, ether_tap_write);
 }
 
 static ssize_t
 ether_tap_read(struct net_device *dev, uint8_t *buf, size_t size)
 {
+    ssize_t len;
+
+    len = read(PRIV(dev)->fd, buf, size);
+    if(len <= 0) {
+        if(len == -1 && errno != EINTR) {
+            errorf("read %s, dev %s ", strerror(errno), dev->name);
+        }
+        return -1;
+    }
+    return len;
 }
 
 static int
 ether_tap_isr(unsigned int irq, void *id)
 {
+    struct net_device *dev;
+    struct pollfd pfd;
+    int ret;
+
+    dev=(struct net_device *)id;
+    pfd.fd =PRIV(dev)->fd;
+    pfd.events=POLLIN;
+    while(1) {
+        ret = poll(&pfd,1,0);
+        if(ret == -1) {
+            if(errno == EINTR) {
+                continue;
+            }
+            errorf("poo %s, dev=%s", strerror(errno), dev->name);
+            return -1;
+        }
+        if(ret==0){
+            break;
+        }
+        ether_input_helper(dev, ether_tap_read);
+    }
+    return 0;
 }
 
 static struct net_device_ops ether_tap_ops = {
@@ -77,4 +169,37 @@ static struct net_device_ops ether_tap_ops = {
 struct net_device *
 ether_tap_init(const char *name, const char *addr)
 {
+    struct net_device *dev;
+    struct ether_tap *tap;
+
+    dev=net_device_alloc();
+    if(!dev) {
+        errorf("alloc error");
+        return NULL;
+    }
+    ether_setup_helper(dev);
+    if (addr) {
+        if(ether_addr_pton(addr, dev->addr) == -1) {
+            errorf("invalid add , add %s", addr);
+            return NULL;
+        }
+    }
+    dev->ops = &ether_tap_ops;
+    tap = memory_alloc(sizeof(*tap));
+    if (!tap){
+        errorf("alloc err");
+        return NULL;
+    }
+    strncpy(tap->name, name, sizeof(tap->name)-1);
+    tap->fd=-1;
+    tap->irq=ETHER_TAP_IRQ;
+    dev->priv=tap;
+    if (net_device_register(dev)==-1) {
+        errorf("net device reg errror");
+        memory_free(tap);
+        return NULL;
+    }
+    intr_request_irq(tap->irq,ether_tap_isr, INTR_IRQ_SHARED, dev->name,dev);
+    infof("ehter device init dev=%s",dev->name);
+    return dev;
 }
